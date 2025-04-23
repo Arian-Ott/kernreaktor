de9fac5a2712e85ca99cb6137c3d6549ce20ae44
Arian <88984301+Arian-Ott@users.noreply.github.com>
2025-04-23 16:30:14 +0200

    feat: add Node model with SQLAlchemy integration for database management
    Signed-off-by: Arian <88984301+Arian-Ott@users.noreply.github.com>

---
+++ api/models/node.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from api.db import Base
+
+class Node(Base):
+    __tablename__ = "nodes"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(64), nullable=False, unique=True, index=True)
+    cpus = Column(Integer, nullable=False)
+    memory = Column(Integer, nullable=False)
+    date_registered = Column(DateTime, nullable=False, default=datetime.now)
+
+
