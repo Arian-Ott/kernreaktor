56d9858db058ba866536174b0a0f99574dd05ade
Arian <88984301+Arian-Ott@users.noreply.github.com>
2025-05-09 11:21:48 +0200

    refactor: update Dockerfile base image and remove deprecated services from docker-compose.yaml; clean up main.py and remove unused routes and services
    Signed-off-by: Arian <88984301+Arian-Ott@users.noreply.github.com>

--- api/main.py
+++ api/main.py
@@ -7,11 +7,10 @@ from api.db import Base, engine
 from api.routes.oauth_routes import router as oauth_router
 from api.routes.user_routes import user_router
 from api.services.startup_service import startup_tasks
-from api.services.mqtt_service import mqtt_listener
-from api.routes.pki_routes import pki_router
-from api.routes.daemon_routes import daemon_router
-from api.routes.mqtt_route import mqtt_router
-from api.routes.pve_routes import pve_router
+
+
+
+
 
 app = FastAPI(debug=settings.DEBUG, root_path="/api/v0")
 
@@ -19,10 +18,10 @@ app = FastAPI(debug=settings.DEBUG, root_path="/api/v0")
 # Routen registrieren
 app.include_router(oauth_router)
 app.include_router(user_router)
-app.include_router(pki_router)
-app.include_router(daemon_router)
-app.include_router(mqtt_router)
-app.include_router(pve_router)
+
+
+
+
 
 
 # Startup Routine
@@ -32,7 +31,7 @@ async def on_startup():
     Base.metadata.create_all(bind=engine)
 
     startup_tasks()
-    asyncio.create_task(mqtt_listener())
+    
 
 
 # Eventhandler einbinden
